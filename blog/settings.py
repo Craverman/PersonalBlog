d25f2e8d176f52a6f81d8f2491ba76e4fcb01ebb
Владислав Захаренков <Craverman>
2022-08-01 22:07:04 +0300

    Creating auth and forms

--- blog/settings.py
+++ blog/settings.py
@@ -158,3 +158,11 @@ CKEDITOR_CONFIGS = {
 }
 
 ###################################
+LOGOUT_REDIRECT_URL = "/"
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'CraVerka@googlemail.com'
+EMAIL_HOST_PASSWORD = ''
