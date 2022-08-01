034cf5ffbefab57acc7771e732aa9e856daf0167
Zakharenkov Vladislav <85833732+Craverman@users.noreply.github.com>
2022-08-02 01:52:05 +0300

    Tags (#5)
    * Creating tags for post
    
    * Finishing stage with tags
    
    Co-authored-by: Владислав Захаренков <Craverman>

--- myblog/views.py
+++ myblog/views.py
@@ -8,6 +8,7 @@ from django.http import HttpResponseRedirect
 from django.http import HttpResponse
 from django.core.mail import send_mail, BadHeaderError
 from django.db.models import Q
+from taggit.models import Tag
 
 
 class MainView(View):
@@ -26,8 +27,12 @@ class MainView(View):
 class PostDetailView(View):
     def get(self, request, slug, *args, **kwargs):
         post = get_object_or_404(Post, url=slug)
+        common_tags = Post.tag.most_common()
+        last_posts = Post.objects.all().order_by('-id')[:5]
         return render(request, 'myblog/post_detail.html', context={
-            'post': post
+            'post': post,
+            'common_tags': common_tags,
+            'last_posts': last_posts
     })
 
 
@@ -118,4 +123,16 @@ class SearchResultsView(View):
             'title': 'Поиск',
             'results': page_obj,
             'count': paginator.count
+        })
+
+    
+class TagView(View):
+    def get(self, request, slug, *args, **kwargs):
+        tag = get_object_or_404(Tag, slug=slug)
+        posts = Post.objects.filter(tag=tag)
+        common_tags = Post.tag.most_common()
+        return render(request, 'myblog/tag.html', context={
+            'title': f'#ТЕГ {tag}',
+            'posts': posts,
+            'common_tags': common_tags
         })
